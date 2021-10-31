c4f7acb0754bac4581a101e1319c5e8f17e837c7
rafaelkenedy <k.rafaelalves@gmail.com>
2021-10-31 00:36:47 -0300

    Typing Cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -36,9 +36,26 @@ export function Dashboard(){
             </Header>
 
             <HighlightCards>
-                <HighlightCard/>
-                <HighlightCard/>
-                <HighlightCard/>
+                <HighlightCard 
+                    type='up'
+                    title='Entradas' 
+                    amount='R$ 17.400,00' 
+                    lastTransaction='Última entrada dia 13 de abril'
+                />
+
+                <HighlightCard
+                    type='down' 
+                    title='Saídas' 
+                    amount='R$ 1.259,00' 
+                    lastTransaction='Última entrada dia 03 de abril'
+                />
+                <HighlightCard
+                    type='total' 
+                    title='Total' 
+                    amount='R$ 16.141,00' 
+                    lastTransaction='1 à 16 de abril'
+                />
+                
             </HighlightCards>
 
         </Container>
