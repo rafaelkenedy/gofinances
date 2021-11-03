0353978e12039a0710b4700b168c9105040bec70
rafaelkenedy <k.rafaelalves@gmail.com>
2021-11-03 02:14:06 -0300

    RectButton e BorderlessButton

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { TouchableOpacityProps } from 'react-native'
+import { RectButtonProps } from 'react-native-gesture-handler'
+
 import {Container, Title} from './styles'
 
-interface Props extends TouchableOpacityProps{
-    title: string
+interface Props extends RectButtonProps{
+    title: string      
 }
-
 
 export function Button({title, ...rest}: Props){
     return(
--- src/components/Form/TransactionTypeButton/styles.ts
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -1,6 +1,7 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled, {css} from 'styled-components/native'
 import { Feather } from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
 
 interface IconProps{
     type: 'up' | 'down'
@@ -11,13 +12,9 @@ interface ContainerProps{
     type: 'up' | 'down'
 }
 
-export const Container = styled.TouchableOpacity<ContainerProps>`
+export const Container = styled.View<ContainerProps>`
     width: 48%;
-    flex-direction: row;
-    align-items: center;
-    padding: 16px;
-    justify-content: center;
-
+    
     border-width: ${({isActive}) => isActive ? 0 : 1.5}px;
     border-style: solid;
     border-color: ${({theme})=> theme.colors.text};
@@ -29,24 +26,22 @@ export const Container = styled.TouchableOpacity<ContainerProps>`
 
     ${({isActive, type})=> isActive && type === 'down' && css`
         background-color: ${({theme}) => theme.colors.attention_light};
-    `}
-
-    
+    `}    
 `;
-
+export const Button = styled(RectButton)`
+    flex-direction: row;
+    align-items: center;   
+    justify-content: center;
+    padding: 16px;
+`;
 export const Icon = styled(Feather)<IconProps>`
     font-size: ${RFValue(24)}px;
     margin-right: 12px;
 
     color: ${({theme, type}) =>
         type === 'up' ? theme.colors.success: theme.colors.attention
-    
-    
-    };
-
-
+     };
 `;
-
 
 export const Title = styled.Text`
     font-family: ${({theme}) => theme.fonts.regular};
